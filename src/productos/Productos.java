5bd9dc8a40f52a95fb639359b1aab392b428553c
alceronDelgado <luis_aceron@soy.sena.edu.co>
2024-11-13 01:08:44 -0500

    retoques

--- src/productos/Productos.java
+++ src/productos/Productos.java
@@ -13,46 +13,55 @@ public class Productos {
 
     public static void main(String[] args) {
         
-        int n = 10,valor = 0;
+        int n = 2,valor = 0;
         String resultado = "", posicion = "";
         String productos[] = new String[n];
         int precios[] = new int[n];
+        String resultadoFinal = " ";
         
 
-        //Ciclo para pedir los nombres de productos
-        for(int i =0; i<productos.length;i++){
-            productos[i] = JOptionPane.showInputDialog("Digite el producto #"+(i+1));
+        //Ciclo para pedir los nombres de productos y sus precios
+        for(int i = 0; i<productos.length;i++){
+           productos[i] = JOptionPane.showInputDialog("Digite el producto #"+(i+1));
+           precios[i] = Integer.parseInt(JOptionPane.showInputDialog("Digite el precio del producto #"+(i+1)));
         }
         
-        
-        //Ciclo para pedir los precios
-        for(int j = 0; j<productos.length;j++){
-            precios[j] = Integer.parseInt(JOptionPane.showInputDialog("Digite el precio del producto #"+(j+1)));
-            
-        }
-        
+
         for(int k = 0;k<precios.length;k++){
             //Si el valor del producto es mayor a 3000, guarde la cantidad de productos mayores a 3000.
             if(precios[k] >=3000){
-                valor = valor+1;
-                //Casteamos el valor para tener la posición del elemento
-                posicion = k + " ";
+                valor += 1;
+                
+                //posicion = k + " ";
             }else{
                 valor = 0;
             }
+            //Casteamos el valor para tener la posición del elemento
+            posicion = k + " ";
+            
+//            resultado +="""
+//                        Productos mayores a $3000 COP"""+valor+" "+
+//                        """
+//                        Producto #"""+(k+1)+productos[k]+" "+
+//                        """
+//                        Precio: $"""+precios[k]+" "+
+//                        """
+//                        Posición: """+posicion+"\n";
             
             resultado +="""
-                        Productos mayores a $3000 COP"""+valor+" "+
+                         PRODUCTO #"""+(k+1)+": "+productos[k]+" "+
                         """
-                        Producto #"""+(k+1)+productos[k]+" "+
+                         PRECIO: $"""+precios[k]+" "+
                         """
-                        Precio: $"""+precios[k]+" "+
-                        """
-                        Posición: """+posicion+"\n";
+                         POSICIÓN: """+posicion+"\n";
 
-
+            
         }
-        JOptionPane.showMessageDialog(null, resultado);
+        resultadoFinal += """
+                        Productos mayores a $3000 COP: """+valor+"\n"+resultado;
+        
+
+        JOptionPane.showMessageDialog(null, resultadoFinal);
     }
     
 }
